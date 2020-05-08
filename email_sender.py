7690df37bd6ecdcab7e82f41c98809581c1b1c3b
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-05-08 23:10:03 +0100

    Created email template in index.html and dynamically set variable value in email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -1,14 +1,24 @@
 import env
 import smtplib
 from email.message import EmailMessage
+# Allows us to get our index.html file and convert it to a template so we can set the variable values
+from string import Template
+from pathlib import Path
+
+# Create a HTML object which is the template of the index.html - use Template and Path to get the path and read as text
+html = Template(Path('index.html').read_text())
 
 # Create an instance of the EmailMessage() class and then configure it to our needs
 email = EmailMessage()
 email['from'] = 'Sunny Hebbar'
 email['to'] = 'sunnyhebbar@hotmail.co.uk'
 email['subject'] = 'Wassuuuuuuuupppppp!'
+
 # Body of the email - can be anything we want (text, images, HTML, etc.)
-email.set_content('Hey dude, how\'s it going?')
+# Here, we want to set the value of the name variable in the index.html file, so we can either do this by stating
+# the kwargs (name='Sunny'), or by using an object and passing in various params ({'name': 'Sunny'})
+# The second set_content argument specifies that the content will be HTML
+email.set_content(html.substitute(name='Sunny'), 'html')
 
 # Use the smtplib client to send the email
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
